db73ab9eb922741b45516d2f5b3a56cffadcc27f
Jefferson Henrique <jhsg.dev@gmail.com>
2020-05-07 03:05:30 -0300

    Adicionado controller

---
+++ src/controllers/OngController.js
@@ -0,0 +1,28 @@
+const crypto = require('crypto')
+const db = require('../database/connection')
+
+module.exports = {
+
+  async index (req, res) {
+    const ongs = await db('ongs').select('*')
+  
+    return res.json(ongs)
+  },
+  
+  async create(req, res) {
+    const {name, email, whatsapp, city, uf} = req.body;
+    const id = crypto.randomBytes(4).toString('HEX');
+  
+    await db('ongs').insert({
+      id,
+      name,
+      email,
+      whatsapp,
+      city,
+      uf
+    })
+  
+    return res.json({id});
+  }
+
+}
--- src/routes.js
+++ src/routes.js
@@ -1,29 +1,9 @@
 const express = require('express')
-const crypto = require('crypto')
+const OngController = require('./controllers/OngController')
 
-const db = require('./database/connection')
 const routes = express.Router()
  
-routes.get('/ongs', async (req, res) => {
-  const ongs = await db('ongs').select('*')
-
-  return res.json(ongs)
-})
-
-routes.post('/ongs', async (req, res) => {
-  const {name, email, whatsapp, city, uf} = req.body;
-  const id = crypto.randomBytes(4).toString('HEX');
-
-  await db('ongs').insert({
-    id,
-    name,
-    email,
-    whatsapp,
-    city,
-    uf
-  })
-
-  return res.json({id});
-})
+routes.get('/ongs', OngController.index)
+routes.post('/ongs', OngController.create)
 
 module.exports = routes;
