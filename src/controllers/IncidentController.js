f7cd74f6c4b6163d7ac12eb2a6ce02d412d95094
dev-jefferson <jhsg.dev@gmail.com>
2020-05-07 20:45:00 -0300

    adicionado novos endpoints para os casos

--- src/controllers/IncidentController.js
+++ src/controllers/IncidentController.js
@@ -1,24 +1,40 @@
-const crypto = require('crypto')
 const db = require('../database/connection')
 
 module.exports = {
 
   async index (req, res) {
-    const ongs = await db('incidents').select('*')
+    const incidents = await db('incidents').select('*')
   
-    return res.json(ongs)
+    return res.json(incidents)
   },
   
   async create(req, res) {
     const {title, description, value} = req.body;
+    const ong_id = req.headers.authorization;
   
-    const result = await db('incidents').insert({
+    const [id] = await db('incidents').insert({
       title,
       description,
-      value
+      value,
+      ong_id
     })
   
-    return res.json(result);
+    return res.json({id});
+  },
+
+  async delete(req, res) {
+    const {id} = req.params;
+    const ong_id = req.headers.authorization;
+
+    const incident = await db('incidents').where('id', id).select('ong_id').first();
+
+    if (incident.ong_id != ong_id) {
+      return res.status(401).json({error: 'Operação não permitida!'})
+    }
+
+    await db('incidents').where('id', id).delete();
+
+    return res.status(204).send();
   }
 
 }
