40af8e96400e3104f706ff1269bc484933ae7f30
dev-jefferson <jhsg.dev@gmail.com>
2020-05-08 11:07:09 -0300

    adionado ongs na listagem de casos

--- src/controllers/IncidentController.js
+++ src/controllers/IncidentController.js
@@ -6,10 +6,20 @@ module.exports = {
     const { page = 1 } = req.query;
 
     const [count] = await db('incidents').count()
-    const incidents = await db('incidents').select('*').limit(5).offset((page - 1) * 5)
+    const incidents = await db('incidents')
+      .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
+      .limit(5).offset((page - 1) * 5)
+      .select([
+        'incidents.*', 
+        'ongs.name',
+        'ongs.email',
+        'ongs.whatsapp',
+        'ongs.city',
+        'ongs.uf',
+      ])
   
     res.header('X-Total-Count', count['count(*)'])
-    
+
     return res.json(incidents)
   },
   
